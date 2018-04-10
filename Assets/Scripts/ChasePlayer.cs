c18e980ac5b8f141232654540c17f0e1ba905b41
MYCOMPUTER\yudai <yudainepnep@icloud.com>
2018-04-10 19:22:52 +0900

    Enemy2がゆっくりとplayerのほうを向くように実装

---
+++ Assets/Scripts/ChasePlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChasePlayer : MonoBehaviour {
+
+    public GameObject player;
+    public float lookPlayerSpeed = 3.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    private void LateUpdate()
+    {
+        LookPlayer();	
+    }
+
+    void LookPlayer()
+    {
+
+        Vector3 targetDir = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z) - transform.position;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, lookPlayerSpeed * Time.deltaTime, 0f);
+        transform.rotation = Quaternion.LookRotation(newDir);
+    }
+}
